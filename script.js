d2194802b564b85ec0f7050791c50cd7ff89df99
Demian Contini <demiandtc@gmail.com>
2026-07-29 20:41:45 -0300

    feat: add Creed and Soul to movies collection

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const movies = [
   {
     title: "Peaceful Warrior",
     image: "assets/images/Peaceful-Warrior.jpg",
-    quote: "The journey begins within.",
-    reflection: "Reflections on the Movie"
+    quote: "There are no ordinary moments",
+    reflection: "This film taught me that discipline and self-awareness matter more than talent."
+  },
+  {
+    title: "Creed",
+    image: "assets/images/Creed.jpg",
+    quote: "One step at a time. One punch at a time. One round at a time.",
+    reflection: "Creed taught me that the past can explain who I am, but it doesn't get to decide who I will become. Every day, I have the opportunity to build my own identity."
+  },
+  {
+    title: "Soul",
+    image: "assets/images/Soul.jpeg",
+    quote: "The spark isn't your purpose",
+    reflection: "Soul reminded me that my purpose isn't hidden in a single passion. Life begins when I become ready to truly live and appreciate the ordinary moments"
   }
 ];
 
